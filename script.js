2b6b0f742e69d4aec7ba1636841a2979d4038154
Kate <chelsea-92@mail.ru>
2020-03-15 03:16:12 +0300

    feat: implement tags switcher

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('#navigation');
+const tags = document.querySelector('#tags');
+const portfolioList = document.querySelector('#portfolio-list');
 
-nav.addEventListener('click', menuSwitcher)
+nav.addEventListener('click', menuSwitcher);
+tags.addEventListener('click', tagsSwitcher);
 
 function menuSwitcher(event) {
   if (event.target.tagName === 'A') {
@@ -9,4 +12,24 @@ function menuSwitcher(event) {
     });
     event.target.classList.add('navigation__link--active');
   }
+}
+
+function tagsSwitcher(event) {
+  if (event.target.tagName === 'SPAN' && !event.target.classList.contains('tags__item--selected')) {
+    tags.querySelectorAll('span').forEach(span => {
+      span.classList.remove('tags__item--selected');
+    });
+    event.target.classList.add('tags__item--selected');
+    shiftPortfolioByOnePosition();
+  }
+}
+
+function shiftPortfolioByOnePosition() {
+  let arrNodes = Array.from(portfolioList.querySelectorAll('.portfolio__item'));
+  arrNodes.unshift(arrNodes.pop());
+
+  portfolioList.innerHTML = '';
+  arrNodes.forEach(node => {
+    portfolioList.append(node)
+  })
 }
